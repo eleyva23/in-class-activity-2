0a73d278c5738712e5b209573a572d7a17e978a6
emma <eleyva2@usf.edu>
2025-09-17 19:50:56 -0700

    creating an array

---
+++ cc_4b.js
@@ -0,0 +1,6 @@
+const product= [
+    {sku: "A-100", name: "iphone", category: "electronics", price: 800.25, inventory: 10},
+    {sku: "B-110", name: "iphone case", category: "accesorries", price: 25.99, inventory: 15},
+    {sku: "C-115", namee: }
+    
+]
