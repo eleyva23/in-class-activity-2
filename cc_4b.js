50908a5a2c262f825365bb99f5127fc4e2e1f73c
emma <eleyva2@usf.edu>
2025-09-17 19:56:06 -0700

    creating products

--- cc_4b.js
+++ cc_4b.js
@@ -1,6 +1,9 @@
 const product= [
     {sku: "A-100", name: "iphone", category: "electronics", price: 800.25, inventory: 10},
     {sku: "B-110", name: "iphone case", category: "accesorries", price: 25.99, inventory: 15},
-    {sku: "C-115", namee: }
+    {sku: "C-115", name: "hairbrush", category: "hair care", price: 10.25, inventory: 5},
+    {sku: "D-120", name: "mascara", category:"makeup", price: 15.00, inventor: 8 },
+    {sku: "c-125", name: "bleach", category: "cleanun supplies", price: 8.75, inventory: 12},
+    ];
+
     
-]
